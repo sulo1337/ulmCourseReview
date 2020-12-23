68a3576d188c8627c9a80a4400d9df6880aff121
sulo1337 <s.acharya1337@gmail.com>
2020-12-22 18:31:52 -0600

    added professor and course models/routes

--- server/routes/course.js
+++ server/routes/course.js
@@ -3,7 +3,27 @@ const router = express.Router();
 const { Course, validateCourse } = require('../models/course');
 
 router.get('/', (req, res) => {
+    Course.find().lean()
+        .then(courses => {
+            return res.status(200).send(courses);
+        })
+        .catch(err => {
+            return res.status(500).send(`Internal server error`);
+        })
+});
 
+router.post('/', async (req, res) => {
+    const { error } = validateCourse(req.body);
+    if (error) return res.status(400).send(`There is an error with course details: \n${JSON.stringify(error.details)}`);
+
+    const course = new Course({
+        cname: req.body.cname,
+        ccode: req.body.ccode,
+        hours: req.body.hours
+    });
+
+    await course.save();
+    res.status(200).send(course);
 })
 
 module.exports = router;
