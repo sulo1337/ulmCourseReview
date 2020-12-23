124efb0209048e7583f651856e05328d215b2a52
sulo1337 <s.acharya1337@gmail.com>
2020-12-22 18:44:10 -0600

    added logging

--- server/index.js
+++ server/index.js
@@ -7,12 +7,16 @@ const dburl = config.get("dburl");
 const student = require('./routes/student.js');
 const course = require('./routes/course.js');
 const professor = require('./routes/professor.js');
+const review = require('./routes/review.js');
 const nopath = require('./middleware/nopath.js');
+const morgan = require('morgan');
 
 app.use(express.json());
+app.use(morgan('combined'));
 app.use('/api/student', student);
 app.use('/api/course', course);
 app.use('/api/professor', professor);
+app.use('/api/review', review);
 app.use(nopath);
 
 mongoose.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true })
